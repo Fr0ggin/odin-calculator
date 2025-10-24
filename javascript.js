9e4a6e3e123011697813e0b59559a4ba0a68b1e7
Fr0ggin <232677958+Fr0ggin@users.noreply.github.com>
2025-10-25 01:22:08 +0200

    fixe issue #1 by creating if statement on func storeInNum

--- javascript.js
+++ javascript.js
@@ -10,20 +10,28 @@ const clear = ((num1, num2, operator) => {
     display_zero.textContent = "0"
 })
 
-
 // bind the variables to the display
 let num1 = document.querySelector(".display_num1")
 let num2 = document.querySelector(".display_num2")
 let operator = document.querySelector(".display_operator")
 let display_zero = document.querySelector(".display_zero")
 
+//initialisation
 clear(num1, num2, operator)
 result = ""
 const operatorlist = ["add", "subtract", "multiply", "divide", "clear", "="]
 
-const storeInNum = (num, input) => num.textContent += input
-const storeOperator = ((input) => {operator.textContent = input})
+// if statement to ensure you dont falsy add nums after result
+const storeInNum = ((num, input) => {
+    if(result != "" && operator.textContent === ""){
+        num.textContent = ""
+        result = ""
+    }
+    num.textContent += input
+    })
 
+
+const storeOperator = ((input) => {operator.textContent = input})
 
 // function that takes an operator and then calls one of the basic math functions
 const operate = ((num1, num2, operator) =>{
@@ -36,22 +44,21 @@ const operate = ((num1, num2, operator) =>{
             return multiply(num1, num2)
         case "/":
             return divide(num1, num2)       
-
     }
 })
+
+
 
 const showResult = (num1, num2, operator) =>{
     // this ensures when pressing double operators nothing get parsed
     if(num2.textContent == ""){return}
     result = operate(num1.textContent, num2.textContent, operator.textContent)
-    
+
     //purely here for funny message on impossible operation (like 0/0 or 2/0)
     if(isNaN(result) || result == "Infinity"){result = "Numnut"}
     num1.textContent = result
     num2.textContent = ""
     operator.textContent = ""
-
-    
 
 }
 const allButtons = document.querySelectorAll("button")
@@ -59,7 +66,7 @@ for(each of allButtons){
     each.addEventListener("click", (e) => {
         if(result === "Numnut"){
             clear(num1, num2, operator)
-            result = 0 
+            result = "" 
         }
 
         else if(operatorlist.includes(e.target.className)){
