087c6d48c8ee1545165f53fb774a9d8519528465
LapTim <232677958+Fr0ggin@users.noreply.github.com>
2025-10-24 18:33:52 +0200

    map the display with variables, and building if on operator

--- javascript.js
+++ javascript.js
@@ -5,9 +5,20 @@ const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
 // variables used for displaying the numbers and operator on display
-let num1 = "";
-let num2 = "";
-let operator = "";
+// let num1 = ""
+// let num2 = ""
+// let operator = ""
+
+// bind the variables to the display
+let num1 = document.querySelector(".display_num1")
+let num2 = document.querySelector(".display_num2")
+let operator= document.querySelector(".display_operator")
+
+num1.textContent = "6"
+num2.textContent = "2"
+operator.textContent = ""
+const operatorlist = ["add", "substract", "multiply", "divide"]
+
 
 // function that takes an operator and then calls one of the basic math functions
 const operate = (num1, num2, operator) => operator(num1, num2);
@@ -15,14 +26,21 @@ const operate = (num1, num2, operator) => operator(num1, num2);
 const allButtons = document.querySelectorAll("button")
 for(each of allButtons){
     each.addEventListener("click", (e) => {
-        console.log("click")
+        console.log("click") // Test to see if clicks are registered, it does!
+        num1.textContent += "1" // Test to see if clicks add, they do!
+
+        if(operatorlist.includes(each)){
+            console.log("you clicked an operator")
+        }
+
+
+ 
+
+        // store numbers on num1 until an operator is pressed
+
     })
 
 }
-// allButtons.addEventListener("click", (e) =>{
-//     console.log("click"); // logs the className of my_element
-// })
-
 // create a function that stores the clicked button in num1
 const storeInNum1 = () => {
 
