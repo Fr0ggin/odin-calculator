908f7f975b798803c3aa86a101de411bc0fd835c
Fr0ggin <232677958+Fr0ggin@users.noreply.github.com>
2025-10-24 22:21:14 +0200

    basic calculator functionality added
    removed extra spaces in button values in html so it can parse correctly
    added case for operate that checks what type of operator and calls the correct function
    all the basic math functions now parse as float

--- javascript.js
+++ javascript.js
@@ -1,48 +1,67 @@
 // Basic functions using the operation as the name
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const add = (num1, num2) => parseFloat(num1) + parseFloat(num2);
+const subtract = (num1, num2) => parseFloat(num1) - parseFloat(num2);
+const multiply = (num1, num2) => parseFloat(num1) * parseFloat(num2);
+const divide = (num1, num2) => parseFloat(num1) / parseFloat(num2);
+const clear = ((num1, num2, operator) => {
+    num1.textContent = ""
+    num2.textContent = ""
+    operator.textContent = ""
+})
 
-// variables used for displaying the numbers and operator on display
-// let num1 = ""
-// let num2 = ""
-// let operator = ""
 
 // bind the variables to the display
 let num1 = document.querySelector(".display_num1")
 let num2 = document.querySelector(".display_num2")
-let operator= document.querySelector(".display_operator")
+let operator = document.querySelector(".display_operator")
 
-num1.textContent = "6"
-num2.textContent = "2"
+num1.textContent = ""
+num2.textContent = ""
 operator.textContent = ""
-const operatorlist = ["+", "-", "*", "/"]
+result = ""
+const operatorlist = ["add", "subtract", "multiply", "divide", "clear", "="]
 
 const storeInNum = (num, input) => num.textContent += input
 const storeOperator = ((input) => {operator.textContent = input})
 
 
 // function that takes an operator and then calls one of the basic math functions
-const operate = (num1, num2, operator) => operator(num1, num2);
+const operate = ((num1, num2, operator) =>{
+    switch(operator){
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1,num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            return divide(num1, num2)       
+
+    }
+})
 
 const allButtons = document.querySelectorAll("button")
 for(each of allButtons){
     each.addEventListener("click", (e) => {
-        // console.log("click") // Test to see if clicks are registered, it does!
-        console.log(e.target)
-        // num1.textContent += "1" // Test to see if clicks add, they do!
-
+        
         if(operatorlist.includes(e.target.className)){
-            console.log("you clicked an operator")
             if(e.target.className === "clear"){
-                //Function that clears all values
+                clear(num1, num2, operator)
+
             }
             else if(e.target.className === "="){
-                // function that displays the result
+                let result = operate(num1.textContent, num2.textContent, operator.textContent)
+                num1.textContent = []
+                num2.textContent = []
+                operator.textContent = result
+            }        
+            else if(operator.textContent === ""){
+                storeOperator(e.target.textContent)
             }
-            else if(operator.textContent =! ""){
-                storeOperator(e.target.className)
+                        
+            else if(operator.textContent != ""){
+                console.log("This is the second operator, we post result here")
+                storeOperator(e.target.textContent)
             }
         }
 
