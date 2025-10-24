573f086a9b9fdbc17f567e41292fafd94d9390fe
Fr0ggin <232677958+Fr0ggin@users.noreply.github.com>
2025-10-24 15:08:52 +0200

    basic calculator functions in console

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// Basic functions using the operation as the name
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+console.log(add(6,2))
+console.log(divide(6,2))
